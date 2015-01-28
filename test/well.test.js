1e082b1f69e9cd4fc686a8f84d31af7c836ec690
Kamil Mech <kamil.mech@nearform.com>
2015-01-28 09:41:53 +0000

    made tester more modular

--- test/well.test.js
+++ test/well.test.js
@@ -6,25 +6,17 @@
 // ADD COPYRIGHT INFO OR A DISCLAIMER
 "use strict";
 
+var Helper 	= require('./test-helper.js')
+var helper = new Helper();
 var _ 		= require('underscore')	// see http://npmjs.org/m/underscore
 var util 	= require('util')
 var assert 	= require('assert')
 var async   = require('async') 		// see http://npmjs.org/m/async
-var seneca  = require('seneca')()
+var seneca  = helper.seneca
 
-// Init well.js
-seneca.use('options','../options.well.js')
-var options = seneca.export('options')
-options.dev_setup = options.well.dev_setup // <- This is not normal, check if app should really work like this
-
-seneca.use('user')
-seneca.use('../well',options)
-
-var userent  = seneca.make('sys/user')
-var teament  = seneca.make('team')
-var eventent  = seneca.make('event')
-
-// Used to create sample data
+var userent  = helper.userent
+var teament  = helper.teament
+var eventent  = helper.eventent
 
 describe('seneca, role:well', function(){
 
@@ -39,7 +31,7 @@ describe('seneca, role:well', function(){
 					code:     'ma',
 					users:    {}
 				},_.omit({name:'MeetupA', code:'ma'},['role','cmd']))).save$( function(err, event){
-					if( err ) return console.log(err)
+					if( err ) return console.error(err)
 					callback()
 				})
 			},
@@ -51,14 +43,14 @@ describe('seneca, role:well', function(){
 					code:     'mb',
 					users:    {}
 				},_.omit({name:'MeetupB', code:'mb'},['role','cmd']))).save$( function(err, event){
-					if( err ) return console.log(err);
+					if( err ) return console.error(err);
 					callback()
 				})
 			},
 			// Loading events from db
 			function(callback){
 				eventent.list$(function(err,events){
-					if( err ) return console.log(err)
+					if( err ) return console.error(err)
 					callback(null, events)
 				})
 			},
@@ -74,7 +66,7 @@ describe('seneca, role:well', function(){
 			      numwells:0,
 			      users:{}
 			    }).save$(function(err, entity){
-			    	if (err) return console.log(err)
+			    	if (err) return console.error(err)
 			        callback(null, events)
 			    })
 			},
@@ -89,7 +81,7 @@ describe('seneca, role:well', function(){
 			      numwells:0,
 			      users:{}
 			    }).save$(function(err, entity){
-			    	if (err) return console.log(err)
+			    	if (err) return console.error(err)
 			    	callback(null, events)
 			    })
 			},
@@ -104,7 +96,7 @@ describe('seneca, role:well', function(){
 			      numwells:0,
 			      users:{}
 			    }).save$(function(err, entity){
-			    	if (err) return console.log(err)
+			    	if (err) return console.error(err)
 			    	callback(null, events)
 			    })
 			},
@@ -114,23 +106,16 @@ describe('seneca, role:well', function(){
 					// Use the cmd:register action of the seneca-user plugin to register the fake users
 					// This ensures they are created properly
 					seneca.act('role:user,cmd:register',{nick:'u'+i,name:'n'+i,password:'p'+i}, function(err, data){
-						if (err) return console.log(err)
+						if (err) return console.error(err)
 						j++
 						if (j < count) return
 
 						callback(null, events)
 					})
 			    }
-			},
-			// Loading users from db
-			function(events, callback){
-				userent.list$(function(err,users){
-					if( err ) return console.log(err)
-					callback(null, events, users)
-				})
 			}],
            function(err, result) {
-				if( err ) return console.log(err)
+				if( err ) return done(err)
 				done()
            }
 		)
@@ -170,7 +155,6 @@ describe('seneca, role:well', function(){
 
 					// Make sure an unwanted element is not contained within the array
 					assert.equal(leader.indexOf('Blue'),-1)
-
 					done()
 				})
 			}
@@ -197,7 +181,7 @@ describe('seneca, role:well', function(){
 			function(events, users, callback){
 				for (var i = 0, j = 0; i < users.length; i++){
 					seneca.act('role:well, cmd:joinevent', {user:users[i], event:events[1]}, function(err, data){
-						if( err ) return console.log(err)
+						if( err ) return done(err)
 						j++
 						if (j < users.length) return
 						callback()
@@ -218,11 +202,11 @@ describe('seneca, role:well', function(){
 					callback(null, teams)
 				})
 			},
-			// Loading users of that team
+			// Loading a known user from that team
 			function(teams, callback){
-				userent.list$({nick:'admin'},function(err,users){
+				userent.load$({nick:'admin'},function(err,user){
 					if( err ) return done(err)
-					callback(null, teams, users[0])
+					callback(null, teams, user)
 				})
 			},
 			// Obtaining members
@@ -245,7 +229,7 @@ describe('seneca, role:well', function(){
 			    	if (teamuser.name != 'admin') dbnames.push(teamuser.name)
 			    })
 
-			    // Storing return members in an array
+			    // Storing returned members in an array
 			    var memnames = []
 			    _.each(members.members,function(member){
 			    	memnames.push(member.name)
@@ -292,54 +276,3 @@ describe('seneca, role:well', function(){
 		done()
 	})
 })
-
-// =========================================================================================================================
-// =================================================Utility functions=======================================================
-
-function list_all(){
-	eventent.list$(function(err,dblist){
-		if( err ) return done(err)
-	  console.log("\n\t\t ---");
-	  console.log("\t\tEvents\n");
-		dblist.forEach(function(element){
-			console.log(element + '\n')
-		})
-	  console.log("\n\t\t ---\n");
-	})
-
-	teament.list$(function(err,dblist){
-		console.log("\n\t\t ---");
-		console.log("\t\tTeams\n");
-		if( err ) return done(err)
-		dblist.forEach(function(element){
-			console.log(element + '\n')
-		})
-		console.log("\n\t\t ---\n");
-	})
-
-	userent.list$(function(err,dblist){
-		console.log("\n\t\t ---");
-		console.log("\t\tUsers\n");
-		if( err ) return done(err)
-		dblist.forEach(function(element){
-			console.log(element + '\n')
-		})
-		console.log("\n\t\t ---\n");
-	})
-}
-
-function showCommands(){
-  console.log("\n\t\t---");
-  console.log("\tSENECA COMMANDS AVAILABLE:\n");
-  seneca.list().forEach(function(element){ console.log(element)})
-  console.log("\n\t\t---\n");
-}
-
-function showRoutes(){
-  seneca.act('role:web, cmd:routes', function(err, routes) {
-    console.log("\n\t\t---");
-    console.log("\tSENECA ROUTES AVAILABLE:\n");
-    console.log(routes);
-    console.log("\n\t\t---\n");
-  });
-}
