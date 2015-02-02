86afed985961305eb581d4447cffd994605b2899
Kamil Mech <kamil.mech@nearform.com>
2015-02-02 10:52:56 +0000

    each test case uses new instance of seneca

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,68 +1,172 @@
 module.exports =
-function(){
-  var seneca  = require('seneca')()
-  this.seneca = seneca
+  function() {
 
-  // Init well.js
-  seneca.use('options','../options.well.js')
-  var options = seneca.export('options')
-  options.dev_setup = options.well.dev_setup // <- This is not normal, check if app should really work like this
+    var async = require('async')
+    var _ = require('lodash')
 
-  seneca.use('user')
-  seneca.use('../well',options)
+    this.seneca = function(done) {
+      var seneca = require('seneca')()
 
-  var userent   = seneca.make('sys/user')
-  this.userent  = userent
-  var teament   = seneca.make('team')
-  this.teament  = teament
-  var eventent  = seneca.make('event')
-  this.eventent = eventent
+      // Init well.js
+      seneca.use('options', '../options.well.js')
+      var options = seneca.export('options')
+      options.dev_setup = options.well.dev_setup // <- This is not normal, check if app should really work like this
 
-  this.list_all = function list_all(){
-    eventent.list$(function(err,dblist){
-      if( err ) return console.error(err)
-      console.log("\n\t\t ---")
-      console.log("\t\tEvents\n")
-      dblist.forEach(function(element){
-        console.log(element + '\n')
+      seneca.use('user')
+      seneca.use('../well', options)
+
+      var userent = seneca.make('sys/user')
+      var teament = seneca.make('team')
+      var eventent = seneca.make('event')
+
+      // Init entities
+      async.waterfall([
+        // Adding events
+        function(callback) {
+          eventent.make$(_.extend({
+              numcards: 52,
+              numteams: 2,
+              name: 'MeetupA',
+              code: 'ma',
+              users: {}
+            }, _.omit({
+              name: 'MeetupA',
+              code: 'ma'
+            }, ['role', 'cmd']))).save$()
+            .make$(_.extend({
+              numcards: 52,
+              numteams: 1,
+              name: 'MeetupB',
+              code: 'mb',
+              users: {}
+            }, _.omit({
+              name: 'MeetupB',
+              code: 'mb'
+            }, ['role', 'cmd']))).save$(function(err, event) {
+              callback(err)
+            })
+        },
+        // Loading events from db
+        function(callback) {
+          eventent.list$(function(err, events) {
+            callback(err, events)
+          })
+        },
+        // Adding teams
+        // Add a team to event with index 0
+        function(events, callback) {
+          teament.make$({
+              num: 0,
+              event: events[0].id,
+              eventcode: events[0].code,
+              name: 'Red',
+              wells: {},
+              numwells: 0,
+              users: {}
+            }).save$()
+            .make$({
+              num: 1,
+              event: events[0].id,
+              eventcode: events[0].code,
+              name: 'Tan',
+              wells: {},
+              numwells: 0,
+              users: {}
+            }).save$(function(err, entity) {
+              callback(err, events)
+            })
+        },
+        // Add a team to a different event
+        function(events, callback) {
+          teament.make$({
+            num: 0,
+            event: events[1].id,
+            eventcode: events[1].code,
+            name: 'Blue',
+            wells: {},
+            numwells: 0,
+            users: {}
+          }).save$(function(err, entity) {
+            callback(err)
+          })
+        },
+        // Load users, but do not assign them to any events to allow tests for custom setup
+        function(callback) {
+          // Use the cmd:register action of the seneca-user plugin to register the fake users
+          // This ensures they are created properly
+          seneca.act('role:user,cmd:register', {
+              nick: 'u1',
+              name: 'n1',
+              password: 'p1'
+            })
+            .act('role:user,cmd:register', {
+              nick: 'u2',
+              name: 'n2',
+              password: 'p2'
+            })
+            .act('role:user,cmd:register', {
+              nick: 'u3',
+              name: 'n3',
+              password: 'p3'
+            })
+            .act('role:user,cmd:register', {
+              nick: 'u4',
+              name: 'n4',
+              password: 'p4'
+            }, callback)
+        }
+      ], function() {
+        done(seneca, userent, teament, eventent)
       })
-      console.log("\n\t\t ---\n");
-    })
+    }
 
-    teament.list$(function(err,dblist){
-      console.log("\n\t\t ---")
-      console.log("\t\tTeams\n")
-      if( err ) return console.error(err)
-      dblist.forEach(function(element){
-        console.log(element + '\n')
+    this.list_all = function list_all(userent, teament, eventent) {
+      eventent.list$(function(err, dblist) {
+        if (err) return console.error(err)
+        console.log("\n\t\t ---")
+        console.log("\t\tEvents\n")
+        dblist.forEach(function(element) {
+          console.log(element + '\n')
+        })
+        console.log("\n\t\t ---\n");
       })
-      console.log("\n\t\t ---\n")
-    })
 
-    userent.list$(function(err,dblist){
-      console.log("\n\t\t ---")
-      console.log("\t\tUsers\n")
-      if( err ) return console.error(err)
-      dblist.forEach(function(element){
-        console.log(element + '\n')
+      teament.list$(function(err, dblist) {
+        console.log("\n\t\t ---")
+        console.log("\t\tTeams\n")
+        if (err) return console.error(err)
+        dblist.forEach(function(element) {
+          console.log(element + '\n')
+        })
+        console.log("\n\t\t ---\n")
       })
-      console.log("\n\t\t ---\n")
-    })
-  }
 
-  this.show_commands = function show_commands(){
-    console.log("\n\t\t---")
-    console.log("\tSENECA COMMANDS AVAILABLE:\n")
-    seneca.list().forEach(function(element){ console.log(element)})
-    console.log("\n\t\t---\n")
-  }
+      userent.list$(function(err, dblist) {
+        console.log("\n\t\t ---")
+        console.log("\t\tUsers\n")
+        if (err) return console.error(err)
+        dblist.forEach(function(element) {
+          console.log(element + '\n')
+        })
+        console.log("\n\t\t ---\n")
+      })
+    }
 
-  this.show_routes = function show_routes(){
-    seneca.act('role:web, cmd:routes', function(err, routes) {
+    this.show_commands = function show_commands(seneca) {
       console.log("\n\t\t---")
-      console.log("\tSENECA ROUTES AVAILABLE:\n");
-      console.log(routes)
+      console.log("\tSENECA COMMANDS AVAILABLE:\n")
+      seneca.list().forEach(function(element) {
+        console.log(element)
+      })
       console.log("\n\t\t---\n")
-    })
+    }
+
+    this.show_routes = function show_routes(seneca) {
+      seneca.act('role:web, cmd:routes', function(err, routes) {
+        console.log("\n\t\t---")
+        console.log("\tSENECA ROUTES AVAILABLE:\n");
+        console.log(routes)
+        console.log("\n\t\t---\n")
+      })
+    }
   }
-}
--- test/well.test.js
+++ test/well.test.js
@@ -6,223 +6,133 @@
 // ADD COPYRIGHT INFO OR A DISCLAIMER
 "use strict";
 
-var Helper   = require('./test-helper.js')
+var Helper = require('./test-helper.js')
 var helper = new Helper();
-var _     = require('lodash')
-var util   = require('util')
-var assert   = require('assert')
-var async   = require('async')
-var seneca  = helper.seneca
+var _ = require('lodash')
+var util = require('util')
+var assert = require('assert')
+var async = require('async')
 
-var userent  = helper.userent
-var teament  = helper.teament
-var eventent  = helper.eventent
-
-describe('seneca, role:well', function(){
-
-  beforeEach(function(done){
-    async.waterfall([
-      // Adding events
-      function(callback){
-        eventent.make$(_.extend({
-          numcards: 52,
-          numteams: 2,
-          name:     'MeetupA',
-          code:     'ma',
-          users:    {}
-        },_.omit({name:'MeetupA', code:'ma'},['role','cmd']))).save$( function(err, event){
-          callback(err)
-        })
-      },
-      function(callback){
-        eventent.make$(_.extend({
-          numcards: 52,
-          numteams: 1,
-          name:     'MeetupB',
-          code:     'mb',
-          users:    {}
-        },_.omit({name:'MeetupB', code:'mb'},['role','cmd']))).save$( function(err, event){
-          callback(err)
-        })
-      },
-      // Loading events from db
-      function(callback){
-        eventent.list$(function(err,events){
-          callback(err, events)
-        })
-      },
-      // Adding teams
-      // Add a team to event with index 0
-      function(events, callback){
-        teament.make$({
-            num:0, 
-            event:events[0].id, 
-            eventcode:events[0].code,
-            name:'Red',
-            wells:{},
-            numwells:0,
-            users:{}
-          }).save$(function(err, entity){
-              callback(err, events)
-          })
-      },
-      // Add another team to event with index 0
-      function(events, callback){
-        teament.make$({
-            num:1, 
-            event:events[0].id, 
-            eventcode:events[0].code,
-            name:'Tan',
-            wells:{},
-            numwells:0,
-            users:{}
-          }).save$(function(err, entity){
+describe('seneca, role:well', function() {
+  it('cmd:leader', function(done) {
+    helper.seneca(function(seneca, userent, teament, eventent) {
+      async.waterfall([
+        // Loading events from db
+        function(callback) {
+          eventent.list$(function(err, events) {
             callback(err, events)
           })
-      },
-      // Add a team to a different event
-      function(events, callback){
-        teament.make$({
-            num:0, 
-            event:events[1].id, 
-            eventcode:events[1].code,
-            name:'Blue',
-            wells:{},
-            numwells:0,
-            users:{}
-          }).save$(function(err, entity){
-            callback(err)
+        },
+        function(events, callback) {
+          // Get list of teams in event 0 through leader cmd
+          seneca.act('role:well, cmd:leader', {
+            event: events[0]
+          }, function(err, leader) {
+            callback(err, events, leader)
           })
-      },
-      // Load users, but do not assign them to any events to allow tests for custom setup
-      function(callback){
-        // Use the cmd:register action of the seneca-user plugin to register the fake users
-        // This ensures they are created properly
-        seneca.act('role:user,cmd:register',{nick:'u1',name:'n1',password:'p1'}, function(err, data){
-          callback(err)
-        })
-      },
-      function(callback){
-        seneca.act('role:user,cmd:register',{nick:'u2',name:'n2',password:'p2'}, function(err, data){
-          callback(err)
-        })
-      },
-      function(callback){
-        seneca.act('role:user,cmd:register',{nick:'u3',name:'n3',password:'p3'}, function(err, data){
-          callback(err)
-        })
-      },
-      function(callback){
-        seneca.act('role:user,cmd:register',{nick:'u4',name:'n4',password:'p4'}, function(err, data){
-          done(err)
-        })
-      }
-    ])
+        },
+        function(events, leader, callback) {
+          // Get list of teams in event 0 directly from db
+          teament.list$({
+            event: events[0].id
+          }, function(err, dbteams) {
+
+            // Format both lists into arrays of names(leader does not contain id data)
+            dbteams = dbteams.map(function(element) {
+              return element.name
+            })
+            leader = leader.teams.map(function(element) {
+              return element.name
+            })
+
+            // Compare team names
+            assert.deepEqual(dbteams, leader)
+
+            // Make sure an unwanted element is not contained within the array
+            assert.equal(leader.indexOf('Blue'), -1)
+            done(err)
+          })
+        }
+      ])
+    })
   })
 
-  it('cmd:leader', function(done){
-
-    async.waterfall([
-      // Loading events from db
-      function(callback){
-        eventent.list$(function(err,events){
+  it('cmd:members', function(done) {
+    helper.seneca(function(seneca, userent, teament, eventent) {
+      async.waterfall([
+        // Loading events from db
+        function(callback) {
+          eventent.list$(function(err, events) {
             callback(err, events)
-        })
-      },
-      function(events, callback){
-        // Get list of teams in event 0 through leader cmd
-        seneca.act('role:well, cmd:leader', {event:events[0]}, function(err, leader){
-          callback(err, events, leader)
-        })
-      },
-      function(events, leader, callback){
-        // Get list of teams in event 0 directly from db
-        teament.list$({event:events[0].id},function(err,dbteams){
-
-          // Format both lists into arrays of names(leader does not contain id data)
-          dbteams = dbteams.map(function (element) {
-            return element.name
           })
-          leader = leader.teams.map(function (element) {
-            return element.name
+        },
+        // Loading users from db
+        function(events, callback) {
+          userent.list$(function(err, users) {
+            callback(err, events, users)
           })
-
-          // Compare team names
-          assert.deepEqual(dbteams, leader)
-
-          // Make sure an unwanted element is not contained within the array
-          assert.equal(leader.indexOf('Blue'),-1)
-          done(err)
-        })
-      }
-    ])
-  })
-
-  it ('cmd:members', function(done){
-    async.waterfall([
-      // Loading events from db
-      function(callback){
-        eventent.list$(function(err,events){
-          callback(err, events)
-        })
-      },
-      // Loading users from db
-      function(events, callback){
-        userent.list$(function(err,users){
-          callback(err, events, users)
-        })
-      },
-      // Insert all users into event 1
-      function(events, users, callback){
-        var count = 0
-        _.each(users, function(user){
-          seneca.act('role:well, cmd:joinevent', {user:user, event:events[1]}, function(err, data){
-            count++
-            if (count < users.length) return
-            callback(err)
+        },
+        // Insert all users into event 1
+        function(events, users, callback) {
+          var count = 0
+          _.each(users, function(user) {
+            seneca.act('role:well, cmd:joinevent', {
+              user: user,
+              event: events[1]
+            }, function(err, data) {
+              count++
+              if (count < users.length) return
+              callback(err)
+            })
           })
-        })
-      },
-      // Loading events from db to refresh data
-      function(callback){
-        eventent.list$(function(err,events){
-          callback(err, events)
-        })
-      },
-      // Loading teams of event B from db
-      function(events, callback){
-        teament.list$({event:events[1].id},function(err,teams){
-          callback(err, teams)
-        })
-      },
-      // Loading a known user from that team
-      function(teams, callback){
-        userent.load$({nick:'admin'},function(err,user){
-          callback(err, teams, user)
-        })
-      },
-      // Obtaining members
-      function(teams, user, callback){
+        },
+        // Loading events from db to refresh data
+        function(callback) {
+          eventent.list$(function(err, events) {
+            callback(err, events)
+          })
+        },
+        // Loading teams of event B from db
+        function(events, callback) {
+          teament.list$({
+            event: events[1].id
+          }, function(err, teams) {
+            callback(err, teams)
+          })
+        },
+        // Loading a known user from that team
+        function(teams, callback) {
+          userent.load$({
+            nick: 'admin'
+          }, function(err, user) {
+            callback(err, teams, user)
+          })
+        },
+        // Obtaining members
+        function(teams, user, callback) {
 
-        seneca.act('role:well, cmd:members', {team:teams[0], user:user}, function(err, members){
-          callback(err, teams, members)
-        })
-      },
-      // Comparing db against members return
-      function(teams, members, callback){
+          seneca.act('role:well, cmd:members', {
+            team: teams[0],
+            user: user
+          }, function(err, members) {
+            callback(err, teams, members)
+          })
+        },
+        // Comparing db against members return
+        function(teams, members, callback) {
 
-        // admin is being removed, because it's supplied into members call as the user to be ignored
+          // admin is being removed, because it's supplied into members call as the user to be ignored
 
-        // Removing admin from list which is db clone
-        // Storing db members in an array
-        var dbnames = []
-          _.each(teams[0].users,function(teamuser){
+          // Removing admin from list which is db clone
+          // Storing db members in an array
+          var dbnames = []
+          _.each(teams[0].users, function(teamuser) {
             if (teamuser.name != 'admin') dbnames.push(teamuser.name)
           })
 
           // Storing returned members in an array
           var memnames = []
-          _.each(members.members,function(member){
+          _.each(members.members, function(member) {
             memnames.push(member.name)
           })
 
@@ -233,37 +143,43 @@ describe('seneca, role:well', function(){
           assert.deepEqual(dbnames, memnames)
 
           callback()
-      }
-    ], done)
+        }
+      ], done)
+    })
   })
 
-  it ('cmd:whoami', function(done){
-    assert.equal('TO BE IMPLEMENTED', 1)
-    // TODO
-    done()
+  it('cmd:whoami', function(done) {
+    helper.seneca(function(seneca, userent, teament, eventent) {
+      // TODO
+      done(new Error('TO BE IMPLEMENTED !'))
+    })
   })
 
-  it ('cmd:well', function(done){
-    assert.equal('TO BE IMPLEMENTED', 1)
-    // TODO
-    done()
+  it('cmd:well', function(done) {
+    helper.seneca(function(seneca, userent, teament, eventent) {
+      // TODO
+      done(new Error('TO BE IMPLEMENTED !'))
+    })
   })
 
-  it ('cmd:member', function(done){
-    assert.equal('TO BE IMPLEMENTED', 1)
-    // TODO
-    done()
+  it('cmd:member', function(done) {
+    helper.seneca(function(seneca, userent, teament, eventent) {
+      // TODO
+      done(new Error('TO BE IMPLEMENTED !'))
+    })
   })
 
-  it ('cmd:createevent', function(done){
-    assert.equal('TO BE IMPLEMENTED', 1)
-    // TODO
-    done()
+  it('cmd:createevent', function(done) {
+    helper.seneca(function(seneca, userent, teament, eventent) {
+      // TODO
+      done(new Error('TO BE IMPLEMENTED !'))
+    })
   })
 
-  it ('cmd:joinevent', function(done){
-    assert.equal('TO BE IMPLEMENTED', 1)
-    // TODO
-    done()
+  it('cmd:joinevent', function(done) {
+    helper.seneca(function(seneca, userent, teament, eventent) {
+      // TODO
+      done(new Error('TO BE IMPLEMENTED !'))
+    })
   })
 })
